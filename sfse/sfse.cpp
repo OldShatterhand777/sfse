bd610254156b025db2561d68027e9ed1e5be025e
OldShatterhand777 <145045918+OldShatterhand777@users.noreply.github.com>
2023-09-15 08:04:25 +0200

    Update sfse.cpp
    doxygen comments

--- sfse/sfse.cpp
+++ sfse/sfse.cpp
@@ -11,162 +11,192 @@
 #include "Hooks_Version.h"
 #include "Hooks_Script.h"
 
+// Global variable to store the module handle.
 HINSTANCE g_moduleHandle = nullptr;
 
+// Function prototypes
 void SFSE_Preinit();
 void SFSE_Initialize();
 
-// api-ms-win-crt-runtime-l1-1-0.dll
-typedef int (*__initterm_e)(_PIFV *, _PIFV *);
-__initterm_e _initterm_e_Original = nullptr;
-
-typedef char * (*__get_narrow_winmain_command_line)();
-__get_narrow_winmain_command_line _get_narrow_winmain_command_line_Original = NULL;
-
-// runs before global initializers
-int __initterm_e_Hook(_PIFV * a, _PIFV * b)
+/**
+ * @brief The entry point for initializing additional scripting capabilities and functionality.
+ *
+ * This function runs before global initializers and can be used for optional plugin preload.
+ *
+ * @param a Pointer to an array of function pointers.
+ * @param b Pointer to another array of function pointers.
+ * @return The result of the original initialization function.
+ */
+int __initterm_e_Hook(_PIFV* a, _PIFV* b)
 {
-	// could be used for plugin optional preload
-
-	SFSE_Preinit();
-
-	return _initterm_e_Original(a, b);
+    SFSE_Preinit();
+    return _initterm_e_Original(a, b);
 }
 
-// runs after global initializers
-char * __get_narrow_winmain_command_line_Hook()
+/**
+ * @brief Hook for retrieving the narrow WinMain command line.
+ *
+ * This function runs after global initializers and performs the usual load-time tasks.
+ *
+ * @return The narrow WinMain command line.
+ */
+char* __get_narrow_winmain_command_line_Hook()
 {
-	// the usual load time
-
-	SFSE_Initialize();
-
-	return _get_narrow_winmain_command_line_Original();
+    SFSE_Initialize();
+    return _get_narrow_winmain_command_line_Original();
 }
 
+/**
+ * @brief Install base hooks for the game.
+ */
 void installBaseHooks(void)
 {
-	DebugLog::openRelative(CSIDL_MYDOCUMENTS, "\\My Games\\" SAVE_FOLDER_NAME "\\SFSE\\Logs\\sfse.txt");
+    // Open a debug log file.
+    DebugLog::openRelative(CSIDL_MYDOCUMENTS, "\\My Games\\" SAVE_FOLDER_NAME "\\SFSE\\Logs\\sfse.txt");
 
-	HANDLE exe = GetModuleHandle(nullptr);
+    // Get the module handle for the executable.
+    HANDLE exe = GetModuleHandle(nullptr);
 
-	// fetch functions to hook
-	auto * initterm = (__initterm_e *)getIATAddr(exe, "api-ms-win-crt-runtime-l1-1-0.dll", "_initterm_e");
-	auto * cmdline = (__get_narrow_winmain_command_line *)getIATAddr(exe, "api-ms-win-crt-runtime-l1-1-0.dll", "_get_narrow_winmain_command_line");
+    // Fetch functions to hook.
+    auto* initterm = (__initterm_e*)getIATAddr(exe, "api-ms-win-crt-runtime-l1-1-0.dll", "_initterm_e");
+    auto* cmdline = (__get_narrow_winmain_command_line*)getIATAddr(exe, "api-ms-win-crt-runtime-l1-1-0.dll", "_get_narrow_winmain_command_line");
 
-	// hook them
-	if(initterm)
-	{
-		_initterm_e_Original = *initterm;
-		safeWrite64(uintptr_t(initterm), u64(__initterm_e_Hook));
-	}
-	else
-	{
-		_ERROR("couldn't find _initterm_e");
-	}
+    // Hook the functions.
+    if (initterm)
+    {
+        _initterm_e_Original = *initterm;
+        safeWrite64(uintptr_t(initterm), u64(__initterm_e_Hook));
+    }
+    else
+    {
+        _ERROR("couldn't find _initterm_e");
+    }
 
-	if(cmdline)
-	{
-		_get_narrow_winmain_command_line_Original = *cmdline;
-		safeWrite64(uintptr_t(cmdline), u64(__get_narrow_winmain_command_line_Hook));
-	}
-	else
-	{
-		_ERROR("couldn't find _get_narrow_winmain_command_line");
-	}
+    if (cmdline)
+    {
+        _get_narrow_winmain_command_line_Original = *cmdline;
+        safeWrite64(uintptr_t(cmdline), u64(__get_narrow_winmain_command_line_Hook));
+    }
+    else
+    {
+        _ERROR("couldn't find _get_narrow_winmain_command_line");
+    }
 }
 
+/**
+ * @brief Wait for a debugger to be attached.
+ */
 void WaitForDebugger(void)
 {
-	while(!IsDebuggerPresent())
-	{
-		Sleep(10);
-	}
+    while (!IsDebuggerPresent())
+    {
+        Sleep(10);
+    }
 
-	Sleep(1000 * 2);
+    Sleep(1000 * 2);
 }
 
+/**
+ * @brief Perform pre-initialization tasks for SFSE.
+ */
 void SFSE_Preinit()
 {
-	static bool runOnce = false;
-	if(runOnce) return;
-	runOnce = true;
+    static bool runOnce = false;
+    if (runOnce)
+        return;
+    runOnce = true;
 
-	SYSTEMTIME now;
-	GetSystemTime(&now);
+    SYSTEMTIME now;
+    GetSystemTime(&now);
 
-	_MESSAGE("SFSE runtime: initialize (version = %d.%d.%d %08X %04d-%02d-%02d %02d:%02d:%02d, os = %s)",
-		SFSE_VERSION_INTEGER, SFSE_VERSION_INTEGER_MINOR, SFSE_VERSION_INTEGER_BETA, RUNTIME_VERSION,
-		now.wYear, now.wMonth, now.wDay, now.wHour, now.wMinute, now.wSecond,
-		getOSInfoStr().c_str());
+    _MESSAGE("SFSE runtime: initialize (version = %d.%d.%d %08X %04d-%02d-%02d %02d:%02d:%02d, os = %s)",
+        SFSE_VERSION_INTEGER, SFSE_VERSION_INTEGER_MINOR, SFSE_VERSION_INTEGER_BETA, RUNTIME_VERSION,
+        now.wYear, now.wMonth, now.wDay, now.wHour, now.wMinute, now.wSecond,
+        getOSInfoStr().c_str());
 
-	_MESSAGE("imagebase = %016I64X", g_moduleHandle);
-	_MESSAGE("reloc mgr imagebase = %016I64X", RelocationManager::s_baseAddr);
+    _MESSAGE("imagebase = %016I64X", g_moduleHandle);
+    _MESSAGE("reloc mgr imagebase = %016I64X", RelocationManager::s_baseAddr);
 
 #ifdef _DEBUG
-	SetPriorityClass(GetCurrentProcess(), IDLE_PRIORITY_CLASS);
+    SetPriorityClass(GetCurrentProcess(), IDLE_PRIORITY_CLASS);
 
-	WaitForDebugger();
+    WaitForDebugger();
 #endif
 
-	if(!g_branchTrampoline.create(1024 * 64))
-	{
-		_ERROR("couldn't create branch trampoline. this is fatal. skipping remainder of init process.");
-		return;
-	}
+    if (!g_branchTrampoline.create(1024 * 64))
+    {
+        _ERROR("couldn't create branch trampoline. this is fatal. skipping remainder of init process.");
+        return;
+    }
 
-	if(!g_localTrampoline.create(1024 * 64, g_moduleHandle))
-	{
-		_ERROR("couldn't create codegen buffer. this is fatal. skipping remainder of init process.");
-		return;
-	}
+    if (!g_localTrampoline.create(1024 * 64, g_moduleHandle))
+    {
+        _ERROR("couldn't create codegen buffer. this is fatal. skipping remainder of init process.");
+        return;
+    }
 
-	// scan plugin folder
-	g_pluginManager.init();
+    // Scan the plugin folder.
+    g_pluginManager.init();
 
-	// preload plugins
-	g_pluginManager.installPlugins(PluginManager::kPhase_Preload);
+    // Preload plugins.
+    g_pluginManager.installPlugins(PluginManager::kPhase_Preload);
 
-	_MESSAGE("preinit complete");
+    _MESSAGE("preinit complete");
 }
 
+/**
+ * @brief Perform initialization tasks for SFSE.
+ */
 void SFSE_Initialize()
 {
-	static bool runOnce = false;
-	if(runOnce) return;
-	runOnce = true;
+    static bool runOnce = false;
+    if (runOnce)
+        return;
+    runOnce = true;
 
-	// load plugins
-	g_pluginManager.installPlugins(PluginManager::kPhase_Load);
-	g_pluginManager.loadComplete();
+    // Load plugins.
+    g_pluginManager.installPlugins(PluginManager::kPhase_Load);
+    g_pluginManager.loadComplete();
 
-	Hooks_Version_Apply();
-	Hooks_Script_Apply();
+    Hooks_Version_Apply();
+    Hooks_Script_Apply();
 
-	FlushInstructionCache(GetCurrentProcess(), NULL, 0);
+    FlushInstructionCache(GetCurrentProcess(), NULL, 0);
 
-	_MESSAGE("init complete");
+    _MESSAGE("init complete");
 
-	DebugLog::flush();
+    DebugLog::flush();
 }
 
+/**
+ * @brief Entry point for starting SFSE.
+ */
 extern "C" {
-	void StartSFSE()
-	{
-		installBaseHooks();
-	}
-	
-	BOOL WINAPI DllMain(HANDLE hDllHandle, DWORD dwReason, LPVOID lpreserved)
-	{
-		switch(dwReason)
-		{
-		case DLL_PROCESS_ATTACH:
-			g_moduleHandle = (HINSTANCE)hDllHandle;
-			break;
+    void StartSFSE()
+    {
+        installBaseHooks();
+    }
 
-		case DLL_PROCESS_DETACH:
-			break;
-		};
+    /**
+     * @brief DllMain function for the SFSE DLL.
+     *
+     * @param hDllHandle The handle to the DLL.
+     * @param dwReason The reason for calling this function.
+     * @param lpreserved Reserved parameter.
+     * @return TRUE if successful, FALSE otherwise.
+     */
+    BOOL WINAPI DllMain(HANDLE hDllHandle, DWORD dwReason, LPVOID lpreserved)
+    {
+        switch (dwReason)
+        {
+        case DLL_PROCESS_ATTACH:
+            g_moduleHandle = (HINSTANCE)hDllHandle;
+            break;
 
-		return TRUE;
-	}
+        case DLL_PROCESS_DETACH:
+            break;
+        };
+
+        return TRUE;
+    }
 }
